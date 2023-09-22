3c992a5616364ce175f8348df2e0a6f49d0b5c05
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-09-22 13:51:20 -0400

    CSS step 1 is ready

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,28 +1,35 @@
 import { React } from 'react'
+import './Step1.css'
 
 export default function Step1 () {
   return (
     <form>
-      <label htmlFor='name'>Name</label>
-      <input
-        id='name'
-        type='text'
-        placeholder='e.g. Stephen King'
-      />
+      <div>
+        <label htmlFor='name'>Name</label>
+        <input
+          id='name'
+          type='text'
+          placeholder='e.g. Stephen King'
+        />
+      </div>
 
-      <label htmlFor='email'>Email Address</label>
-      <input
-        id='email'
-        type='text'
-        placeholder='e.g. stephenking@lorem.com'
-      />
+      <div>
+        <label htmlFor='email'>Email Address</label>
+        <input
+          id='email'
+          type='text'
+          placeholder='e.g. stephenking@lorem.com'
+        />
+      </div>
 
-      <label htmlFor='phone'>Phone Number</label>
-      <input
-        id='phone'
-        type='text'
-        placeholder='e.g. +1 234 567 890'
-      />
+      <div>
+        <label htmlFor='phone'>Phone Number</label>
+        <input
+          id='phone'
+          type='text'
+          placeholder='e.g. +1 234 567 890'
+        />
+      </div>
     </form>
   )
 }
